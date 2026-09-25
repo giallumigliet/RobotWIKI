62eb9bdac61f0ced8541157f68a29be1f205159e
Gianluca Miglietta <163427589+giallumigliet@users.noreply.github.com>
2026-09-25 16:01:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1383,6 +1383,29 @@ searchInput.addEventListener(
 );
 
 
+
+
+
+function showHome() {
+    document
+        .querySelectorAll(".page")
+        .forEach(page => page.classList.add("hidden"));
+
+    document
+        .getElementById("home")
+        .classList.remove("hidden");
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+
+
+
+
+
 /*
 ==================================================
 AVVIO
